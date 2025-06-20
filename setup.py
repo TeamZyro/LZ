8a9c1ef163ec63b03cb39fbe34602e8b002f7056
Tᴇᴀᴍ【ＺＹＲＯ】 <harshmanjhi0004@gmail.com>
2025-06-20 14:01:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='LZ',
-    version='1.0',
+    name="LZ",
+    version="0.1.0",
     packages=find_packages(),
-    description='Custom sudo UID manager',
-    author='Team Zyro',
-    author_email='support@teamzyro.com',
     install_requires=[],
-    zip_safe=False
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A library for identifying anime characters from images",
+    keywords="anime, character, identification, telegram",
+    url="https://github.com/yourusername/namebot",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.7",
 )
