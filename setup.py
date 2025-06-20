9f060ff0f8b4939a28bb10081242c8267c3f88f4
Tᴇᴀᴍ【ＺＹＲＯ】 <harshmanjhi0004@gmail.com>
2025-06-20 13:37:28 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="LZ",
+    version="0.1",
+    packages=find_packages(),
+    description="Gives sudo power to specific user ID.",
+    author="YourName",
+    author_email="your@email.com",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
