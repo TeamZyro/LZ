3bba951a4f5b1ab146972755e38a0670c073af1d
Tᴇᴀᴍ【ＺＹＲＯ】 <harshmanjhi0004@gmail.com>
2025-06-20 14:01:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="LZ",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=["pyrogram"],
     author="Your Name",
     author_email="your.email@example.com",
     description="A library for identifying anime characters from images",
